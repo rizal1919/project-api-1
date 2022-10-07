e41b7435b3b4670667261f7ceccc3d3e012dde64
rizal1919 <rizalfathurr1999@gmail.com>
2022-10-07 07:50:31 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,80 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/api',  function(){
+        $uri="https://apijkn-dev.bpjs-kesehatan.go.id/vclaim-rest-dev/referensi/propinsi";
+       
+        $data = "5306";
+        $secretKey = "8wXDF487C5";
+              // Computes the timestamp
+               date_default_timezone_set('UTC');
+               $tStamp = strval(time()-strtotime('1970-01-01 00:00:00'));
+                // Computes the signature by hashing the salt with the secret key as the key
+        $signature = hash_hmac('sha256', $data."&".$tStamp, $secretKey, true);
+        // var_dump($signature);
+        
+        // base64 encode�
+        $encodedSignature = base64_encode($signature);
+        // $encodedAuthorization = base64_encode($pcareUname.':'.$pcarePWD.':'.$kdAplikasi);
+        
+
+        $json = [
+            "kodekelas" => "VIP", 
+            "koderuang" => "RG01", 
+            "namaruang" => "Ruang Anggrek VIP", 
+            "kapasitas" => "20", 
+            "tersedia" => "10",
+            "tersediapria" => "0", 
+            "tersediawanita" => "0", 
+            "tersediapriawanita" => "0"
+        ];
+
+
+        // $response = $this->withHeaders([
+           
+        // ])->get($uri, $json);
+      
+       
+        $ch = curl_init();
+        $headers = array(
+            "X-cons-id: " . $data . "",
+            "X-timestamp: " . $tStamp . "",
+            "X-signature:" . $encodedSignature ."",
+            "Content-Type: Application/json",
+        );
+
+        // // set url 
+        curl_setopt($ch, CURLOPT_URL, $uri); 
+        curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers); 
+        $data = curl_exec($ch);
+        curl_close($ch);
+        
+        // $this->assertStatus(200);
+
+        $client = new GuzzleHttp\Client();
+        $res = $client->request('GET', $uri, [
+            'headers' => [
+                'Accept'     => 'application/json',
+                // "Content-Type" => "application/json",
+                "X-cons-id" => $data,
+                "X-timestamp" => $tStamp,
+                "X-signature" => $encodedSignature,
+                "X-authorization" => base64_encode('doasdasdsa')
+            ],
+        ]);
+
+        dd($res);
+
+        echo $res->getStatusCode();
+        // "200"
+        echo $res->getHeader('content-type')[0];
+        // 'application/json; charset=utf8'
+        echo $res->getBody();
+        // {"type":"User"...'
+
+        
+});
--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,6 +3,9 @@
 namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use Illuminate\Support\Facades\Http;
+use Illuminate\Testing\TestResponse;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
@@ -17,9 +20,10 @@ class ExampleTest extends TestCase
         // $response = $this->get('/');
 
         // $response->assertStatus(200);
-
-        $uri="https://apijkn-dev.bpjs-kesehatan.go.id/vclaim-rest-dev/aplicaresws/rest/bed/update/0193R004";
         
+        $uri="https://apijkn-dev.bpjs-kesehatan.go.id/vclaim-rest-dev/referensi/propinsi";
+        
+       
         $data = "5306";
         $secretKey = "8wXDF487C5";
               // Computes the timestamp
@@ -27,37 +31,60 @@ class ExampleTest extends TestCase
                $tStamp = strval(time()-strtotime('1970-01-01 00:00:00'));
                 // Computes the signature by hashing the salt with the secret key as the key
         $signature = hash_hmac('sha256', $data."&".$tStamp, $secretKey, true);
+        // var_dump($signature);
         
         // base64 encode�
         $encodedSignature = base64_encode($signature);
+        // $encodedAuthorization = base64_encode($pcareUname.':'.$pcarePWD.':'.$kdAplikasi);
         
-        // urlencode�
-        // $encodedSignature = urlencode($encodedSignature);
-        echo "\n";
-        echo "X-cons-id: " .$data ." \n";
-        echo "X-timestamp:" .$tStamp ." \n";
-        echo "X-signature: " .$encodedSignature;
 
-        $headers = array( 
-            "Accept: application/json", 
-            "X-cons-id:".$data ." ", 
-            "X-timestamp: ".$tStamp ." ", 
-            "X-signature: ".$encodedSignature
-        ); 
+        $json = [
+            "kodekelas" => "VIP", 
+            "koderuang" => "RG01", 
+            "namaruang" => "Ruang Anggrek VIP", 
+            "kapasitas" => "20", 
+            "tersedia" => "10",
+            "tersediapria" => "0", 
+            "tersediawanita" => "0", 
+            "tersediapriawanita" => "0"
+        ];
 
 
-        // 
-        // $ch = curl_init();
+        // $response = $this->withHeaders([
+        //     "Content-Type" => "application/json",
+        //     "X-cons-id" => $data,
+        //     "X-timestamp" => $tStamp,
+        //     "X-signature" => $encodedSignature
+        // ])->get($uri, $json);
+      
+       
+        $ch = curl_init();
+        $headers = array(
+            "X-cons-id: " . $data . "",
+            "X-timestamp: " . $tStamp . "",
+            "X-signature:" . $encodedSignature ."",
+            "X-authorization:" . base64_encode('doasdasdsa') . "",
+            "Content-Type: application/json",
+        );
 
         // // set url 
-        // curl_setopt($ch, CURLOPT_URL, $uri); 
-        // curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-        // curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
-        // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        // curl_setopt($ch, CURLOPT_HTTPHEADER, $headers); 
-        // $data = curl_exec($ch);
+        curl_setopt($ch, CURLOPT_URL, $uri); 
+        curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+        // curl_setopt($curl, CURLOPT_POST, true);
+        // curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
+        // curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers); 
+        $data = curl_exec($ch);
+        echo $data;
         
-        // curl_close($ch);
+
+        $this->assertTrue(true);
+
+        // dd($data);
+
+        
         
         // kode ppk : 0193R004 
         // const id : 5306
